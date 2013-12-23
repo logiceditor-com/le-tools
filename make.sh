cfc8491a441f96f5fb7bd3ec4933f3920eda96cb
Alexander Gladysh <agladysh@gmail.com>
2013-12-23 19:11:02 +0400

    make.sh: no longer using deprecated xargs option
    Fixes the script for OS X.

--- make.sh
+++ make.sh
@@ -3,9 +3,10 @@
 set -e
 
 echo "----> Removing rocks"
-luarocks list | grep le-tools | sudo xargs -l1 luarocks remove --force || true
+luarocks list | grep le-tools | sudo xargs -L1 luarocks remove --force || true
 
 echo "----> Making rocks"
+# Note: order is important here
 sudo luarocks make rockspec/le-tools.le-lua-interpreter-scm-1.rockspec
 sudo luarocks make rockspec/le-tools.le-call-lua-module-scm-1.rockspec
 
