f4d42106679ba36d0a05b96e897fa4df157677a7
Vladimir Dronnikov <dronnikov@gmail.com>
2014-04-30 09:50:24 +0400

    pivot/format: typo fixed

--- src/lua/pivot/format.lua
+++ src/lua/pivot/format.lua
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
--- run.lua: tool for making pivot tables from tab-separated data
+-- format.lua: pivot table serializers
 -- This file is a part of le-tools library
 -- Copyright (c) le-tools authors (see file `COPYRIGHT` for the license)
 --------------------------------------------------------------------------------
